ef6c7e6eff2d5e099bce34b535f816e5330bb9bf
praphawanpp-sys <praphawan.pp@gmail.com>
2026-09-16 15:59:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -449,8 +449,7 @@ elif selected_menu == t_ui["m3"]:
     with st.form("add_new_item_form"):
             new_code = st.text_input("รหัสสินค้า (Product Code)")
             new_name = st.text_input("ชื่อสินค้า (Item Name)")
-            new_supplier = st.selectbox("ชื่อร้านค้า / Supplier (เช่น Makro, Gourmet Market)",
-            ["Makro", "Gourmet Market", "Lotus's", "Big C", "อื่นๆ"]  # หรือใช้ตัวแปรเก็บรายชื่อซัพพลายเออร์ของคุณ เช่น st.session_state.suppliers_list)
+            new_supplier = st.selectbox("ชื่อร้านค้า / Supplier", st.session_state.suppliers_list if "suppliers_list" in st.session_state else ["Makro", "Gourmet Market"]))
             new_category = st.selectbox("หมวดหมู่สินค้า", st.session_state.categories_list)
             new_unit = st.selectbox("หน่วยนับ", st.session_state.units_list)
             new_conv = st.number_input("อัตราส่วนการแปลงหน่วย (Conversion Qty)", value=1.0, min_value=0.01)
