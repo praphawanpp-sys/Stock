25642334648afdeeff6ac96a3fb2c605c5f0b12e
praphawanpp-sys <praphawan.pp@gmail.com>
2026-09-16 15:41:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -480,8 +480,8 @@ elif selected_menu == t_ui["m3"]:
                     st.success(f"✨ เพิ่มสินค้า '{new_name}' สำเร็จเรียบร้อยแล้ว!")
                     st.rerun()
 
-    with tab_manage_unit:
-    st.subheader("จัดการหน่วยนับ (Units)")
+        with tab_manage_unit:
+        st.subheader("จัดการหน่วยนับ (Units)")
     
     # --- ตัวอย่างฟอร์มเพิ่มหน่วยนับใหม่ ---
     with st.form("add_unit_form"):
